7fc4d59e7be02f8f3b08165cdde48da37896d16c
Øyvind Jonassen <ojo@skagenfondene.no>
2019-10-09 22:21:55 +0200

    Styling fixes on beerlist

---
+++ beers.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+Template Name: Beers
+*/
+ 
+?>
+<?php get_header(); ?>
+
+<link href=/css/app.bcaeb796.css rel=preload as=style>
+  <link href=/js/app.4f92d0b6.js rel=preload as=script>
+  <link href=/js/chunk-vendors.4d43d6c7.js rel=preload as=script>
+  <link href=/css/app.bcaeb796.css rel=stylesheet>
+
+
+<?php include(get_template_directory()."/units/navbar.php"); ?>
+        <div class="page-search-and-filter page-standard page">
+          <div class=container>
+            <div class=main-section>
+              <div id=app></div>
+            </div>
+          </div>
+        </div>
+        <script src=/js/chunk-vendors.4d43d6c7.js> </script> <script src=/js/app.4f92d0b6.js> </script>        
+<?php get_footer(); ?>
--- src/components/BeerList.vue
+++ src/components/BeerList.vue
@@ -1,58 +1,86 @@
 <template>
-<div>
+  <div>
     <div class="card-columns">
-        <div v-for="(beer, index) in beers" :item="beer" :key="index" class="card">
-            <div class="beer">
-                <div class="row">
-                    <div class="col-2">
-                        <div :class="(beer.styleshort) + ' left-bar'">  
-                    </div>
-                    </div>
-                    <div class="col-10">
-                        <div :class="(beer.styleshort) + ' beer-content'">
-                            <h4>{{beer.breweryName}}</h4>
-                            <h3 class="title">{{ beer.name }}</h3>
-                            <h2 class="beer-alc">{{ beer.abv }}%</h2>
-                            <div class="beer-style">
-                                <span>{{ beer.style }}</span>
-                            </div>
-                            <div>
-                                <img src="../assets/time.svg" />
-                                <span class="beer-tapsession">
-                                    <span v-if="beer.untappdSectionId === '440439'">Fri 14-18</span>
-                                    <span v-else-if="beer.untappdSectionId === '440440'">Fri 18-22</span>
-                                    <span v-else-if="beer.untappdSectionId === '440449'">Sat 14-18</span>
-                                    <span v-else-if="beer.untappdSectionId === '440450'">Sat 18-22</span>
-                                </span>
-                            </div>
-                            <div>
-                                <img  src="../assets/location.svg" />
-                                <span class="beer-location">{{ beer.room }}</span>
-                            </div>
-                        </div>
-                    </div>
+      <div v-for="(beer, index) in beers" :item="beer" :key="index" class="card">
+        <div class="beer">
+          <div :class="(beer.styleshort) + ' beer-content'">
+            <div class="row">
+              <div class="col-12">
+                <div class="line-bottom padding">
+                  <h3 class="title">{{ beer.name }}</h3>
                 </div>
+              </div>
             </div>
+            <div class="row">
+              <div class="col-12">
+                <div class="line-bottom">
+                  <div class="row">
+                    <div class="col-4 no-padding-right">
+                      <div class="padding right">
+                        <h2 class="beer-alc">
+                          {{ beer.abv }}
+                          <span class="alc">%</span>
+                        </h2>
+                      </div>
+                    </div>
+                    <div class="col-8 no-padding-left">
+                      <div class="padding extra">
+                        <h4>{{beer.breweryName}}</h4>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <div class="line-bottom padding">
+                  <div class="beer-style">
+                    <span>{{beer.style}}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <div class="row">
+                  <div class="col-6 no-padding-right">
+                    <div class="padding right">
+                      <span class="beer-location">{{ beer.room }}</span>
+                    </div>
+                  </div>
+                  <div class="col-6 no-padding-left text-right">
+                    <div class="padding">
+                      <span class="beer-tapsession">
+                        <span v-if="beer.untappdSectionId === '440439'">Friday early</span>
+                        <span v-else-if="beer.untappdSectionId === '440440'">Friday late</span>
+                        <span v-else-if="beer.untappdSectionId === '440449'">Saturday early</span>
+                        <span v-else-if="beer.untappdSectionId === '440450'">Saturday late</span>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>  
-
-</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'BeerList',
-    props: {
-    },
-    computed: {
-        beers() {
-            return this.$store.getters.GetBeersForList;
-        },
-    },
-}
+  name: "BeerList",
+  props: {},
+  computed: {
+    beers() {
+      return this.$store.getters.GetBeersForList;
+    }
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
